b8ff426bf1ef9b32cea34879b8d36187e2fbb340
hayatkhan67 <hayatkhan626225@outlook.com>
2025-02-23 22:51:21 +0500

    Refactor data initialization in recent_pdf_controller and downgrade shared_preferences dependency

--- lib/controller/recent_pdf_controller.dart
+++ lib/controller/recent_pdf_controller.dart
@@ -1,13 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:pdf_viewer_app/services/shared_prefs_service/shared_prefs_service.dart';
 
 ValueNotifier<int> counter = ValueNotifier<int>(0); // ValueListenable object
 
-initializeData() {
-  log(Prefs().getCounter().toString());
-  counter.value = Prefs().getCounter();
+initializeData() async {
+  counter.value = await Prefs().getCounter();
 }
 
 setData() {
